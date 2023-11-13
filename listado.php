d643e0e9c76e20b1fb50f2478aaf732dbab78277
dudwcs <148365696+dudwcs@users.noreply.github.com>
2023-11-13 12:20:52 +0100

    Update listado.php

--- listado.php
+++ listado.php
@@ -74,7 +74,7 @@ scale=1.0, minimum-scale=1.0">
     <!-- css para usar Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <title>Tema 3</title>
+    <title>Tema 4- Tarea04.1</title>
 </head>
 
 <body>
